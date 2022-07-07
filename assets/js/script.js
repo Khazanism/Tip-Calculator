b1874f7ad2aa5dbb5ccf95b5f1cf77261361555f
Khazanins <khazanyassin@gmail.com>
2022-07-07 22:14:04 +0200

    Changed CSS , Add Evend listener in Js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,20 @@
 //Tip Calculator
 
+
+// Add event listener
+
+const sliders = document.querySelectorAll("input[type='range']");
+
+sliders.forEach(function name(sliders) {
+  sliders.addEventListener("input", calculateTip);
+});
+
+const billInput = document.getElementById("bill");
+billInput.addEventListener("change", calculateTip);
+console.log(billInput);
+
+// Get input values
+
+function calculateTip() {
+
+}
