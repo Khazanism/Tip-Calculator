66234466607d5b2b9960c0811fa6f25ce4bc01e4
Khazanins <khazanyassin@gmail.com>
2022-07-07 23:13:06 +0200

    Get input values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,24 @@
 //Tip Calculator
 
-
 // Add event listener
 
 const sliders = document.querySelectorAll("input[type='range']");
 
-sliders.forEach(function name(sliders) {
+sliders.forEach(function(sliders){
   sliders.addEventListener("input", calculateTip);
 });
 
 const billInput = document.getElementById("bill");
 billInput.addEventListener("change", calculateTip);
-console.log(billInput);
+// console.log(billInput);
 
 // Get input values
 
 function calculateTip() {
+  let bill = parseFloat(billInput.value);
+  let tipPercent = document.getElementById("tip").value;
+  let noOfPeople = document.getElementById("no-of-people").value;
 
+  billInput.value = bill.toFixed(2);
+//   console.log(bill, tipPercent, noOfPeople);
 }
